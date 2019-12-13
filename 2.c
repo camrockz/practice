ee69f86459f94529dac0db6ff1b1f3cf12560bd8
camrockz <55742787+camrockz@users.noreply.github.com>
2019-12-13 23:15:13 +1000

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,33 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <math.h>
-
-float get_float()
-{
-    char buffer[10];
-    char *endp;
-    errno = 0;
-    fgets(buffer ,  sizeof(buffer) , stdin);
-    float ret_float = strtof(buffer , &endp);
-    if (*endp != '\n')
-    {
-        fprintf(stderr , "Invalid input\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (errno != 0 && ret_float == 0)
-    {
-        perror("strtof");
-        exit(EXIT_FAILURE);
-    }
-    if (endp == (char*)&buffer)
-    {
-        fprintf(stderr , "There were no numbers found");
-        exit(EXIT_FAILURE);
-    }
-    return ret_float;
-}
+#include "libpractice.h"
 
 int main()
 {
