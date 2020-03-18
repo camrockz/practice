d31f157fa3f2e79ad4760a37e8d9822dbfa53e2c
camrockz <camrockz@gmail.com>
2020-03-18 18:23:06 +1000

    200318

---
+++ 6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "practice.h"
+
+int sumOfDigits(int);
+
+int main()
+{
+    printf("Type in a number: ");
+	int digits = get_int();
+	int sum = sumOfDigits(digits);
+	printf("%d\n" , sum);
+   return 0;
+   
+}
+
+int sumOfDigits(int digits)
+{
+	if (digits > 0)
+		{
+		return ((digits%10) + (sumOfDigits(digits/10)));
+		}
+	else return 0;
+}
