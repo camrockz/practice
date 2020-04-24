fdfcf597c244e3074e115602c98ec999c7793c20
camrockz <camrockz@gmail.com>
2020-04-25 00:28:37 +1000

    20200425

---
+++ 7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "practice.h"
+#include <string.h>
+
+
+int main()
+{
+	int length = 100;
+	char buffer[length];
+	getString(length , buffer);
+	char **ppbuff =  mkstrArray(buffer);
+	int j = 0;
+	while (ppbuff[j] != NULL)
+	{
+		printf("%s\n" , ppbuff[j]);
+		j++;
+	}
+	free(*ppbuff);
+	return 0;
+}
+
